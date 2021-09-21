664cd84a0f1c9277f92b8bcd7a72d438489dd9ae
Marcosl14 <marcos_l14@hotmail.com>
2021-09-21 19:28:29 -0300

    Change atributes names adding underscore and add properties with getters and setters

--- server/src/domain/entities/rate.entity.ts
+++ server/src/domain/entities/rate.entity.ts
@@ -1,34 +1,96 @@
+import shortId from 'shortid';
+import Technology from './technology.entity';
+import LanguageEnum from '../enums/language.enum';
+import SeniorityEnum from '../enums/seniority.enum';
+
 class Rate {
-  private id: number;
-  private technology_id: number;
-  private seniority: string;
-  private languaje: string;
-  private average_salary: string;
-  private gross_margin: string;
-  private currency: string;
+  private _id: string;
+  private _technology: Technology;
+  private _seniority: SeniorityEnum;
+  private _languaje: LanguageEnum;
+  private _averageSalary: string;
+  private _grossMargin: string;
+  private _currency: string;
 
   constructor(
-    technology_id: number,
-    seniority: string,
-    languaje: string,
-    average_salary: string,
-    gross_margin: string,
+    technology: Technology,
+    seniority: SeniorityEnum,
+    languaje: LanguageEnum,
+    averageSalary: string,
+    grossMargin: string,
     currency: string,
   ) {
-    this.technology_id = technology_id;
-    this.seniority = seniority;
-    this.languaje = languaje;
-    this.average_salary = average_salary;
-    this.gross_margin = gross_margin;
-    this.currency = currency;
+    this._id = shortId.generate();
+    this._technology = technology;
+    this._seniority = seniority;
+    this._languaje = languaje;
+    this._averageSalary = averageSalary;
+    this._grossMargin = grossMargin;
+    this._currency = currency;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public set id(pid: string) {
+    this._id = pid;
+  }
+
+  public get technology(): Technology {
+    return this._technology;
+  }
+
+  public set technology(ptechnology: Technology) {
+    this._technology = ptechnology;
+  }
+
+  public get seniority(): SeniorityEnum {
+    return this._seniority;
+  }
+
+  public set seniority(pseniority: SeniorityEnum) {
+    this._seniority = pseniority;
+  }
+
+  public get languaje(): LanguageEnum {
+    return this._languaje;
+  }
+
+  public set languaje(planguaje: LanguageEnum) {
+    this._languaje = planguaje;
+  }
+
+  public get averageSalary(): string {
+    return this._averageSalary;
+  }
+
+  public set averageSalary(paverageSalary: string) {
+    this._averageSalary = paverageSalary;
+  }
+
+  public get grossMargin(): string {
+    return this._grossMargin;
+  }
+
+  public set grossMargin(pgrossMargin: string) {
+    this._grossMargin = pgrossMargin;
+  }
+
+  public get currency(): string {
+    return this._currency;
+  }
+
+  public set currency(pcurrency: string) {
+    this._currency = pcurrency;
   }
 
   public getTotal(): number {
-    return parseFloat(this.gross_margin) + parseFloat(this.average_salary);
+    return parseFloat(this._grossMargin) + parseFloat(this._averageSalary);
   }
 
   public getGrossMarginPercentage(): number {
-    return parseFloat(this.gross_margin) / this.getTotal();
+    return parseFloat(this._grossMargin) / this.getTotal();
   }
 }
 export default Rate;
