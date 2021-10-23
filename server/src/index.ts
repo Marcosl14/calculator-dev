c8c7d846874bef16b9602b7f0212ff5ad2809616
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-23 17:13:07 -0300

    Create new salaries routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import expressWinston from 'express-winston';
 import winston from 'winston';
 import CommonRoutes from './http/routes/common.routes';
 import RatesRoutes from './http/routes/rates.routes';
+import SalaryRoutes from './http/routes/salary.routes';
 
 const app: express.Application = express();
 
@@ -26,6 +27,7 @@ app.use(cors());
 app.use(express.json());
 
 routes.push(new RatesRoutes(app));
+routes.push(new SalaryRoutes(app));
 
 app.listen(3000, () => {
   routes.forEach((route: CommonRoutes) => {
