daa44dd921edf4bc2d8eb07f0498207440baf57e
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-23 17:15:14 -0300

    Add Seniority and Language verification

--- server/src/application/handlers/rate/create.rate.handler.ts
+++ server/src/application/handlers/rate/create.rate.handler.ts
@@ -1,4 +1,6 @@
 import Rate from '../../../domain/entities/rate.entity';
+import LanguageEnum from '../../../domain/enums/language.enum';
+import SeniorityEnum from '../../../domain/enums/seniority.enum';
 import rateRepository from '../../../infrastructure/repositories/rate.repository';
 import technologyRepository from '../../../infrastructure/repositories/technology.repository';
 import { CreateRateCommand } from '../../commands/rate/create.rate.command';
@@ -6,26 +8,29 @@ import { CreateRateCommand } from '../../commands/rate/create.rate.command';
 class CreateRateHandler {
   async execute(command: CreateRateCommand) {
     const technology = await technologyRepository.findOneById(command.getTechnology());
-
     if (!technology) {
       throw new Error('Technology not found');
     }
 
-    const rateExists: Boolean = await rateRepository.exists(
-      technology,
-      command.getSeniority(),
-      command.getLanguage(),
-      command.getCurrency(),
-    );
+    const seniority = Object.values(SeniorityEnum).find(key => key == command.getSeniority().toUpperCase());
+    if (!seniority) {
+      throw new Error('Seniority not found');
+    }
 
+    const language = Object.values(LanguageEnum).find(key => key == command.getLanguage().toUpperCase());
+    if (!language) {
+      throw new Error('Language not found');
+    }
+
+    const rateExists: Boolean = await rateRepository.exists(technology, seniority, language, command.getCurrency());
     if (rateExists) {
       throw new Error('Rate already exists');
     }
 
     const rate: Rate = new Rate(
       technology,
-      command.getSeniority(),
-      command.getLanguage(),
+      seniority,
+      language,
       command.getAverageSalary(),
       command.getGrossMargin(),
       command.getCurrency(),
