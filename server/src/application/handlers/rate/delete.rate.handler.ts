7c4509ad6a24d537b2591b2631e68f5cf78292f3
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-04 18:31:16 -0300

    Delete rate handler adapted to database

--- server/src/application/handlers/rate/delete.rate.handler.ts
+++ server/src/application/handlers/rate/delete.rate.handler.ts
@@ -1,15 +1,13 @@
-import Rate from '../../../domain/entities/rate.entity';
 import rateRepository from '../../../infrastructure/repositories/rate.repository';
 import { DeleteRateCommand } from '../../commands/rate/delete.rate.command';
 
 class DeleteRateHandler {
   async execute(command: DeleteRateCommand) {
-    const rate: Rate | null = await rateRepository.findOneById(command.getId());
-
-    if (!rate) {
+    try {
+      await rateRepository.deleteById(command.getId());
+    } catch {
       throw new Error('Rate not found');
     }
-    await rateRepository.deleteById(command.getId());
   }
 }
 
