2e68ff733362f0ea7b28140b2063be1fa22a7364
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-04 20:37:02 -0300

    Reorder Schemas for _id renaming

--- server/src/infrastructure/repositories/schemas/rate.schema.ts
+++ server/src/infrastructure/repositories/schemas/rate.schema.ts
@@ -1,18 +1,23 @@
 import { ObjectId } from 'bson';
 import { Schema } from 'mongoose';
+import TechnologySchema from './technology.schema';
 
 const RateSchema = new Schema({
-  technology: {
-    type: {
-      _id: ObjectId,
-      name: String,
-    },
-  },
+  _id: { type: ObjectId },
+  technology: TechnologySchema,
   seniority: { type: String },
   language: { type: String },
   averageSalary: { type: String },
   grossMargin: { type: String },
   currency: { type: String },
+});
+
+RateSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (_doc, ret) {
+    delete ret._id;
+  },
 });
 
 export default RateSchema;
--- server/src/infrastructure/repositories/schemas/technology.schema.ts
+++ server/src/infrastructure/repositories/schemas/technology.schema.ts
@@ -1,7 +1,17 @@
+import { ObjectId } from 'bson';
 import { Schema } from 'mongoose';
 
 const TechnologySchema = new Schema({
+  _id: { type: ObjectId },
   name: { type: String },
+});
+
+TechnologySchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (_doc, ret) {
+    delete ret._id;
+  },
 });
 
 export default TechnologySchema;
