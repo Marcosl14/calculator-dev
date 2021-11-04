e1f9d2160d37c6337e2459fddf2dbc2938e89702
Marcos Giordano <marcos_l14@hotmail.com>
2021-11-04 18:31:58 -0300

    Change repository name

--- server/src/application/handlers/technology/delete.technology.handler.ts
+++ server/src/application/handlers/technology/delete.technology.handler.ts
@@ -1,15 +1,15 @@
 import Technology from '../../../domain/entities/technology.entity';
-import technologyRepository from '../../../infrastructure/repositories/technology.repository';
+import TechnologyRepository from '../../../infrastructure/repositories/technology.repository';
 import { DeleteTechnologyCommand } from '../../commands/technology/delete.technology.command';
 
 class DeleteTechnologyHandler {
   async execute(command: DeleteTechnologyCommand) {
-    const technology: Technology | null = await technologyRepository.findOneById(command.getId());
+    const technology: Technology | null = await TechnologyRepository.findOneById(command.getId());
 
     if (!technology) {
       throw new Error('Technology not found');
     }
-    await technologyRepository.deleteById(command.getId());
+    await TechnologyRepository.deleteById(command.getId());
   }
 }
 
